f68a5bb169209817b8e9edf5a060ab1d27eea347
gdoza5 <gdoza5@gmail.com>
2020-03-01 13:40:38 -0600

    auth and admin middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,7 +56,24 @@ exports.requireSignin = expressJwt({
     userProperty: "auth"
 });
 
+exports.isAuth = (req, res, next ) => {
+    let user = req.profile && req.auth && req.profile._id == req.atuh._id
+        if(!user) {
+            return res.status(403).json({
+                error: "Access denied"
+            });
+        }
+        next();
+};
 
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0) {
+        return res.return(403).json({
+            error: "Admin resource! Access denied"
+        });
+    }
+    next();
+}
 
 
 

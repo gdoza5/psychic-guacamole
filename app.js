9a665a767d7a656f4f3a1b5d43bdf7694df141c6
gdoza5 <gdoza5@gmail.com>
2020-02-29 21:11:09 -0600

    user middleware

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator')
 require('dotenv').config();
 //import routes
-const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 //app
 const app = express();
@@ -27,7 +28,8 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 //routes middleware
-app.use("/api", userRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000
 

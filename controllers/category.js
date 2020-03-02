0c2dcd6d3286a4955aff917a642993ed951bf1aa
gdoza5 <gdoza5@gmail.com>
2020-03-02 17:42:24 -0600

    category

---
+++ controllers/category.js
@@ -0,0 +1,13 @@
+const Category = require("../models/category");
+
+exports.create = () => {
+    const category = new Category(req.body)
+    category.save((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({ data });
+    });
+};

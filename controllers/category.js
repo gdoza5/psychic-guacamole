a3a49c3537c0091a6b146cd3b5b9b31e05f17ea0
gdoza5 <gdoza5@gmail.com>
2020-03-26 16:50:39 -0500

    category read

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,18 @@
 const Category = require("../models/category");
 
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !Category) {
+            return res.status(400).json({
+                error: 'Category does not exist'
+            });
+        }
+        req.category = category;
+        next();
+    });
+};
+
 exports.create = (req, res) => {
     const category = new Category(req.body);
     category.save((err, data) => {
@@ -11,3 +24,8 @@ exports.create = (req, res) => {
         res.json({ data });
     });
 };
+
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+}
